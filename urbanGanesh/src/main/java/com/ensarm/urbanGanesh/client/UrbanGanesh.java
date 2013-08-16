b0b749620cda6c6c3d5955b182e2d9877378666a
nairutee <nairutee@ensarm.com>
2013-08-16 11:20:22 +0530

    Initial commit - raw project

---
+++ urbanGanesh/src/main/java/com/ensarm/urbanGanesh/client/UrbanGanesh.java
@@ -0,0 +1,18 @@
+package com.ensarm.urbanGanesh.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class UrbanGanesh implements EntryPoint {
+ 
+  /**
+   * This is the entry point method.
+   */
+  public void onModuleLoad() {
+	  Window.alert("Welcome to urban Ganesh");
+  }
+}
